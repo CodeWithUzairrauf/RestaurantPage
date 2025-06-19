211cc0a7bf8a104c8f85b911bdbbd7b4addc776a
UzairRauf <uzairrauf006@gmail.com>
2025-06-19 20:17:44 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+
+console.log('Webpack is running!');
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
